127fb59711a6170e10c8f4f837f9e99504ea221a
NaijasonEzeiru <67031753+NaijasonEzeiru@users.noreply.github.com>
2022-10-10 03:55:10 +0100

    Fixed navbar

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var x = document.getElementById("navlinks")
 var container = document.getElementById("container")
 var na = document.getElementById("na")
+var home = document.getElementById("home")
 
 // close the navbar when the user clicks on any of the nav links
 
@@ -8,6 +9,7 @@ function closeNav() {
     x.className = "close";
     container.className = "container"
     na.style.height = "unset"
+    home.style.padding = "48px"
 }
 
 // toggles nav bar open/close 
@@ -16,6 +18,7 @@ function openNav() {
     x.className = "open"
     container.className = "opaque"
     na.style.height = "0px"
+    home.style.padding = "104px 48px 48px 48px"
 }
 
 // close navbar when the user scrolls 
